ead76694f2b037f3f2319fdbc8f3db44e77d37a3
Chidalubo <129227410+Chidalubo@users.noreply.github.com>
2024-01-20 17:59:37 -0700

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,13 @@
 -- Table: Categories
-CREATE TABLE Categories (
+CREATE TABLE Categories
+(
   category_id INT PRIMARY KEY,
   category_name VARCHAR(255)
 );
 
 -- Table: Products
-CREATE TABLE Products (
+CREATE TABLE Products
+(
   product_id INT PRIMARY KEY,
   product_name VARCHAR(255),
   description TEXT,
@@ -15,7 +17,8 @@ CREATE TABLE Products (
 );
 
 -- Table: Users
-CREATE TABLE Users (
+CREATE TABLE Users
+(
   user_id INT PRIMARY KEY,
   username VARCHAR(255),
   email VARCHAR(255),
@@ -25,7 +28,8 @@ CREATE TABLE Users (
 );
 
 -- Table: Orders
-CREATE TABLE Orders (
+CREATE TABLE Orders
+(
   order_id INT PRIMARY KEY,
   user_id INT,
   order_date DATE,
@@ -34,7 +38,8 @@ CREATE TABLE Orders (
 );
 
 -- Table: Order_Items
-CREATE TABLE Order_Items (
+CREATE TABLE Order_Items
+(
   order_item_id INT PRIMARY KEY,
   order_id INT,
   product_id INT,
@@ -45,7 +50,8 @@ CREATE TABLE Order_Items (
 );
 
 -- Table: Reviews
-CREATE TABLE Reviews (
+CREATE TABLE Reviews
+(
   review_id INT PRIMARY KEY,
   user_id INT,
   product_id INT,
@@ -57,14 +63,16 @@ CREATE TABLE Reviews (
 );
 
 -- Table: Cart
-CREATE TABLE Cart (
+CREATE TABLE Cart
+(
   cart_id INT PRIMARY KEY,
   user_id INT,
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 -- Table: Cart_Items
-CREATE TABLE Cart_Items (
+CREATE TABLE Cart_Items
+(
   cart_item_id INT PRIMARY KEY,
   cart_id INT,
   product_id INT,
@@ -74,7 +82,8 @@ CREATE TABLE Cart_Items (
 );
 
 -- Table: Payments
-CREATE TABLE Payments (
+CREATE TABLE Payments
+(
   payment_id INT PRIMARY KEY,
   order_id INT,
   payment_date DATE,
@@ -84,7 +93,8 @@ CREATE TABLE Payments (
 );
 
 -- Table: Shipping
-CREATE TABLE Shipping (
+CREATE TABLE Shipping
+(
   shipping_id INT PRIMARY KEY,
   order_id INT,
   shipping_date DATE,
